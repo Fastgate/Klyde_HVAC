a5ce7dff36078ffd17df7be8015c1044c35a12b5
Fastgate <fastgate@web.de>
2021-03-27 15:05:57 +0100

    start

---
+++ Can.h
@@ -0,0 +1,79 @@
+#ifndef CAN_H
+#define CAN_H
+
+#include <arduinoIO.h>
+#include <FlexCAN.h>
+#include "Serial.h"
+
+struct CanData {
+  union {
+    unsigned char data[4];
+    BitFieldMember<0, 32> canId;
+  } metaData;
+  uint8_t rxBuf[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+};
+
+class CanInput : public Input {
+  public:
+    CanInput(uint32_t canId, uint8_t byteNumber, uint8_t bitMask) {
+      this->canId       = canId;
+      this->byteNumber  = byteNumber;
+      this->bitMask     = bitMask;
+    }
+    virtual boolean getState() { 
+      return this->state;
+    }
+    void update1(CAN_message_t canMessage) {
+      if (canMessage.id == this->canId) {
+        this->state = canMessage.len >= this->byteNumber + 1 && (canMessage.buf[this->byteNumber] & this->bitMask) == this->bitMask;
+      }
+    }
+  private:
+    uint32_t canId      = 0;
+    uint8_t byteNumber  = 0;
+    uint8_t bitMask     = B00000000;
+    
+    boolean state       = false;
+};
+
+class Obd2Helper {
+  public:
+  void sendRequest(uint8_t mode, uint8_t pid) {
+     CAN_message_t requestMessage;
+     requestMessage.id = 0x7DF;
+     requestMessage.buf[0] = 2;
+     requestMessage.buf[1] = mode;
+     requestMessage.buf[2] = pid;
+     Can0.write(requestMessage);
+  }
+};
+
+class CanSniffer {
+  public:
+    CanSniffer() {
+      this->canSnifferPacket = new SerialDataPacket<CanData>(0x62, 0x6d);
+    }
+    void toggle(boolean state) {
+      this->isActive = state;
+    }
+    void update(CAN_message_t canMessage) {
+      if (!this->isActive) {
+        return; 
+      }
+
+      this->canSnifferPacket->payload()->metaData.canId = canMessage.id;
+      for (uint8_t i = 0; i < 8; i++) {
+        uint8_t data = 0x00;
+        if (i < canMessage.len) {
+           data = canMessage.buf[i];
+        }
+        this->canSnifferPacket->payload()->rxBuf[i] = data;
+      }
+      this->canSnifferPacket->serialize(Serial);
+    }
+  private:
+    SerialDataPacket<CanData> *canSnifferPacket;
+    boolean isActive = false;
+};
+
+#endif
---
+++ Klyde_HVAC.ino
@@ -0,0 +1,150 @@
+#include <SPI.h>
+#include <FlexCAN.h>
+#include <arduinoIO.h>
+
+
+#include "Binary.h"
+#include "Serial.h"
+#include "Hvac.h"
+
+#include "Can.h"
+
+  /////////////
+ // HELPERS //
+/////////////
+
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define max(X, Y)  ((X) > (Y) ? (X) : (Y))
+
+
+  /////////////////////
+ // MMI DEFINITIONS //
+/////////////////////
+
+//SerialDataPacket<uint8_t> _MmiAndroidPacket(0x73, 0x72);
+
+  ////////////////////////////
+ // CAR MODULE DEFINITIONS //
+////////////////////////////
+
+Hvac hvac;
+
+
+
+  ////////////////////////
+ // SERIAL DEFINITIONS //
+////////////////////////
+
+SerialPacket statusInitSuccess(0x61, 0x01);
+SerialPacket statusInitError(0x65, 0x01);
+SerialDataPacket<unsigned long> baudRateChange(0x65, 0x01);
+
+SerialReader serialReader(128);
+
+
+  /////////////////////////
+ // CAN BUS DEFINITIONS //
+/////////////////////////
+
+CanSniffer canSniffer;
+Obd2Helper obd2;
+
+
+
+  //////////////////
+ // SKETCH SETUP //
+//////////////////
+
+void setup() {
+
+  delay(1000);  
+  Serial.begin(115200);
+  delay(500);
+  statusInitSuccess.serialize(Serial);
+
+  Can0.begin(500000);
+
+
+  SPI.begin();
+
+}
+
+
+  /////////////////
+ // SKETCH LOOP //
+/////////////////
+
+void loop() {
+  
+
+  hvac.update();
+  
+  
+}
+
+
+  //////////////////
+ // SERIAL EVENT //
+//////////////////
+
+void serialEvent() {
+  serialReader.read(Serial, readSerial);
+}
+
+void readSerial(uint8_t type, uint8_t id, BinaryBuffer *payloadBuffer) {
+  switch (type) {
+    case 0x61:
+      switch (id) {
+        case 0x0a: // start sniffer
+          canSniffer.toggle(true);
+          break;
+        case 0x0b: // stop sniffer
+          canSniffer.toggle(false);
+          break;
+        case 0x72: { // set baud rate
+            BinaryData::LongResult result = payloadBuffer->readLong();
+            if (result.state == BinaryData::OK) {
+              baudRateChange.payload(htonl(result.data));
+              baudRateChange.serialize(Serial);
+              Serial.flush();
+              Serial.end();
+              Serial.begin(result.data);
+              while (Serial.available()) {
+                Serial.read();
+              }
+            }
+            break;
+          }
+      }
+      break;
+    case 0x63:
+      hvac.write(id, payloadBuffer);
+      break;
+  }
+}
+
+
+  ///////////////////
+ // MMI FUNCTIONS //
+///////////////////
+
+
+
+
+  ///////////////////////
+ // CAN BUS FUNCTIONS //
+///////////////////////
+
+void updateCan() { 
+  CAN_message_t canMessage;
+ 
+  while (Can0.available()) {
+    Can0.read(canMessage);
+    canSniffer.update(canMessage);
+    
+
+    
+  }
+  
+  //Serial.println(FLDoorSensor.getState());
+}
